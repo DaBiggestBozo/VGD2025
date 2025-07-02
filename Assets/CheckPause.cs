30f85351372a17f4e1c5e8544cfb37060f23ce55
Kraai <1724803@mukilteo.wednet.edu>
2025-07-02 12:33:04 -0700

    adding junk

---
+++ Assets/CheckPause.cs
@@ -0,0 +1,38 @@
+using Palmmedia.ReportGenerator.Core;
+using UnityEngine;
+
+public class CheckPause : MonoBehaviour
+{
+    [SerializeField] private Pause pause;
+    [SerializeField] private SensitivtySettings sensSettings;
+    [SerializeField] private VolumeSettings volumeSettings;
+    [SerializeField] private PlayerCam playerCam;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Escape)) //Alpha 1 (one key above keyboard) exists for testing, as unity does not register escape inputs in editor mode
+        {
+            checkMenuAction();
+        }
+    }
+
+    void checkMenuAction() //check whether to open or close a menu, and which one
+    {
+        if (settingsPanel.activeInHierarchy)
+        {
+            sensSettings.SavePrefs();
+            volumeSettings.SavePrefs();
+            pauseMenu.SetActive(true);
+            settingsPanel.SetActive(false);
+            playerCam.GetSens();
+        } else if (pauseMenu.activeInHierarchy)
+        {
+            pause.Resume();
+        } else
+        {
+            pause.StartPause();
+        }
+    }
+}
